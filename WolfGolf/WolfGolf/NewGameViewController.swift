4143684052b03e6c7bed5dc25dc72d53e1bc7da7
Brian Gardner <briangardner@brians-mbp.lan>
2020-07-07 15:47:30 -0500

    adding newGame VC

---
+++ WolfGolf/WolfGolf/NewGameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewGameViewController.swift
+//  WolfGolf
+//
+//  Created by Brian Gardner on 7/7/20.
+//  Copyright © 2020 gardner. All rights reserved.
+//
+
+import UIKit
+
+class NewGameViewController: UIViewController {
+    
+    
+    @IBOutlet weak var addPlayerLabel: UILabel!
+    @IBOutlet weak var playerTableView: UITableView!
+    @IBOutlet weak var addPlayerButton: UIButton!
+    @IBOutlet weak var newPlayerTextField: UITextField!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
