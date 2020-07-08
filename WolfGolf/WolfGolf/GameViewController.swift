0ad778b3f875e6f698dd63489ae597ae266013ec
Brian Gardner <briangardner@brians-mbp.lan>
2020-07-08 16:03:08 -0500

    idek

--- WolfGolf/WolfGolf/GameViewController.swift
+++ WolfGolf/WolfGolf/GameViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
+    
+//    func playGame() {
+//        
+//        for round in numRounds {
+//            
+//        }
+//    }
     
 
     /*
